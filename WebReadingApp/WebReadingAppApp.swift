7188a31364035fd537764912d99b2163c93981c2
Shashank Gautam <shashank320722@gmail.com>
2026-01-28 15:55:53 +0530

    Initial Commit

---
+++ WebReadingApp/WebReadingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebReadingAppApp.swift
+//  WebReadingApp
+//
+//  Created by Shashank Gautam on 28/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WebReadingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
